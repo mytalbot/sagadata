da35228d9971b4152d0a0e4bfc4a87bd768d2c49
Talbot <talbot.steven@mh-hannover.de>
2019-06-08 18:03:53 +0200

    First commit

---
+++ R/sagadata.R
@@ -0,0 +1,17 @@
+#' SAGA Data
+#'
+#' These are the SAGA core data for model building. You'll need them loaded when using saga. Each of the 92 arrays provides 39428 probes with
+#' array intensity information. Enjoy!
+#'
+#' @docType data
+#'
+#' @usage data(sagadata)
+#'
+#' @format A 39428 x 92 data frame with post-op data. E
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(sagadata)
+#' head(sagadata)
+"sagadata"
