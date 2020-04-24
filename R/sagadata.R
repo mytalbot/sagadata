1112c4ebb43107cfb8a5d30c6804691ca892c442
Talbot <talbot.steven@mh-hannover.de>
2020-04-24 09:36:09 +0200

    Updata SAGA data (model sample size increase to 152)

--- R/sagadata.R
+++ R/sagadata.R
@@ -1,17 +1,17 @@
 #' SAGA Data
 #'
-#' These are the SAGA core data for model building. You'll need them loaded when using saga. Each of the 92 arrays provides 39428 probes with
+#' These are the SAGA core data for model building. You'll need them loaded when using saga. Each of the arrays provides 39428 probes with
 #' array intensity information. Enjoy!
 #'
 #' @docType data
 #'
-#' @usage data(sagadata)
+#' @usage data(SAGA_Data)
 #'
-#' @format A 39428 x 92 data frame with post-op data. E
+#' @format A 39428 x 153 data frame with SAGA data.
 #'
 #' @keywords datasets
 #'
 #' @examples
-#' data(sagadata)
-#' head(sagadata)
-"sagadata"
+#' data(SAGA_Data)
+#' head(SAGA_Data)
+"SAGA_Data"
